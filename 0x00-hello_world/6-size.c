ea775ddd0e70c519f14eda0df40c633609addcb3
Rilwan <akinyusuf5@gmail.com>
2022-03-10 14:31:27 -0800

    c-syntax

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of a char: 0 byte(s)
+", sizeof(char));
+printf("Size of an int: 0 byte(s)
+", sizeof(int));
+printf("Size of a long int: 0 byte(s)
+", sizeof(long int));
+printf("Size of a long long int: 0 byte(s)
+", sizeof(long long int));
+printf("Size of a float: 0 byte(s)
+", sizeof(float));
+return (0);
+}
