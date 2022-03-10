0a6462b5d66270925ac95965e4548608ca003945
Rilwan <akinyusuf5@gmail.com>
2022-03-10 15:00:08 -0800

    formatting

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,10 @@
  */
 int main(void)
 {
-printf("Size of a char: 0 byte(s)
-", sizeof(char));
-printf("Size of an int: 0 byte(s)
-", sizeof(int));
-printf("Size of a long int: 0 byte(s)
-", sizeof(long int));
-printf("Size of a long long int: 0 byte(s)
-", sizeof(long long int));
-printf("Size of a float: 0 byte(s)
-", sizeof(float));
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
 return (0);
 }
