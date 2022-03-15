a7006ef819bba677346bbbbc9c08a42e28ea0b81
Rilwan <akinyusuf5@gmail.com>
2022-03-16 00:52:22 +0100

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - Entry block
+ * Description: prints '_putchar on new line
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_putchar');
+    _putchar('\n');
+	
+	return (0);
+}
