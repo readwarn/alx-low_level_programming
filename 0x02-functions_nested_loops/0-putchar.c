fbf428d0a57cd084547d9ad6484448ba657de1ff
Rilwan <akinyusuf5@gmail.com>
2022-03-16 01:21:59 +0100

    new cmt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,9 @@
  * Return: 0
  */
 int main(void)
-{  
+{
 char word[8] = '_putchar'
-for(int i = 0; i < 8; i++){
-_putchar(word[i]);
-}
+for( int i = 0; i < 8; i++) _putchar(word[i]);
 _putchar('\n');
 return (0);
 }
