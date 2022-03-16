c778ba26b79f761bb67c3b0c813eefa492a7e515
Rilwan <akinyusuf5@gmail.com>
2022-03-16 01:48:17 +0100

    the end

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,28 +1,13 @@
-/* _putchar.c */
 int _putchar(char c);
-
-/*10 x alphabet */
 void print_alphabet(void);
-
-/* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
-
 int _islower(int c);
-
-int _isalpha(int n);
-
+int _isalpha(int c);
 int print_sign(int n);
-
-int _abs(int n);
-
-int print_last_digit(int n);
-
+int _abs(int);
+int print_last_digit(int);
 void jack_bauer(void);
-
 void times_table(void);
-
-int add(int a, int b);
-
+int add(int, int);
 void print_to_98(int n);
-
 void print_times_table(int n);
